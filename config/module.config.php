656ec1a386870efabb042c3e330b2f9787470e8d
Jaap van Otterdijk <jaap@youngguns.nl>
2014-03-07 10:55:38 +0100

    added username, password

--- config/module.config.php
+++ config/module.config.php
@@ -5,6 +5,8 @@ return array(
         'clientname' => 'yourclient',
         'transport' => 'Oauth',
         'transport_options' => array(
+            'username' => null,
+            'password' => null,
             'consumer_key' => null,
             'consumer_secret' => null,
             'token' => null,
